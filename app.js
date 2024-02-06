666ab2c11962711005ee5e4c22a927a6280ccd2a
Ganesh Pandey <64975510+GaneshPandey-GP@users.noreply.github.com>
2024-02-07 00:20:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dbConnection from './dbConnection.js';
 import routes from './routes.js';
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Middleware to parse JSON requests
 app.use(express.json());
